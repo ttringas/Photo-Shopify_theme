6e8a3d0dd2436b10187f4809a7ef900e299c4c16
Tyler Tringas <ttringas@gmail.com>
2012-01-23 20:19:14 -0500

    working slider and photo switcher

---
+++ assets/theme.js
@@ -0,0 +1,36 @@
+$(document).ready(function(){
+   
+   // Fire up the bxSlider http://www.bxslider.com/
+   var slider = $('#collection_preview').bxSlider({
+        mode: 'vertical',
+        displaySlideQty: 7,
+        moveSlideQty: 1,
+        controls: false,
+        margin: 10
+   });
+   
+   // set arrows to control slider
+   $('#up_arrow').click(function(e){
+     slider.goToPreviousSlide();
+     return false;
+   });
+   
+   $('#down_arrow').click(function(e){
+     slider.goToNextSlide();
+     return false;
+   });
+   
+   //correct weird positioning of slider ####possibly need to fix this later
+   $('.bx-wrapper').css(
+     {'position' : 'absolute', 'right' : '0', 'top' : '182px'}
+     );
+   
+    $('#collection_preview a').click(function() {
+        $('#collection_main_image a').attr('href', $(this).attr('href')).data('rel', $(this).data('rel'));
+        $('#collection_main_image img').attr('src', $(this).children('img').attr('src'));
+        return false;
+    });
+   
+    
+   
+ });
